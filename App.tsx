c33fd015bb524b0727b4e8f10d87e9d6e70f6cd8
Marlon Marques <marlon.belohd@gmail.com>
2022-11-14 18:14:22 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {StatusBar, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <StatusBar barStyle={'dark-content'} />
+      <Text>The Coffee</Text>
+    </View>
+  );
+};
+
+export default App;
