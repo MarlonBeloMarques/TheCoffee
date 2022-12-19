4b130786f6df5cafdd77c09ca3a42daae906b80e
Marlon Marques <marlon.belohd@gmail.com>
2022-12-19 20:17:36 -0300

    chore(assets/images): add coffee images to assets

---
+++ src/presentation/assets/images/index.ts
@@ -0,0 +1,5 @@
+export { default as CoffeeImage1 } from './coffee_1.png';
+export { default as CoffeeImage2 } from './coffee_2.png';
+export { default as CoffeeImage3 } from './coffee_3.png';
+export { default as CoffeeImage4 } from './coffee_4.png';
+export { default as CoffeeImage5 } from './coffee_5.png';
