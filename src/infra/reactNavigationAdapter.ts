fe5596b7ceddf6ab94930e20d45d44cc6ae460fa
Marlon Marques <marlon.belohd@gmail.com>
2023-01-28 16:18:43 -0300

    feat(infra): move reactNavigationAdapter to your folder and refactor test

---
+++ src/infra/reactNavigationAdapter.ts
@@ -0,0 +1,13 @@
+import {
+  CommonActions,
+  NavigationContainerRef,
+} from '@react-navigation/native';
+import { NavigateScreen } from '~/data/navigate';
+
+export default class ReactNavigationAdapter implements NavigateScreen {
+  constructor(readonly navigation: NavigationContainerRef<any>) {}
+
+  navigate(routeName: string, params?: GenericObject | undefined): void {
+    this.navigation.dispatch(CommonActions.navigate(routeName, params));
+  }
+}
--- tests/infra/navigate/reactNavigationAdapter.spec.tsx
+++ tests/infra/navigate/reactNavigationAdapter.spec.tsx
@@ -3,36 +3,36 @@ import {
   CommonActions,
   NavigationContainerRef,
 } from '@react-navigation/native';
-import { render } from '@testing-library/react-native';
-import { NavigateScreen } from '~/data/navigate';
+import { render, waitFor } from '@testing-library/react-native';
 import { Navigation, Routes } from '~/main/navigation';
+import { ReactNavigationAdapter } from '~/infra';
 
 describe('Infra: ReactNavigationAdapter', () => {
-  test('should call dispatch navigate action of React Navigation when call navigate', () => {
-    let navigation = {} as NavigationContainerRef<any>;
-    const navigateSpy = jest.spyOn(CommonActions, 'navigate');
-
-    render(
-      <Navigation
-        setNavigationTop={(navigationRef) => (navigation = navigationRef)}
-        initialRouteName={Routes.WELCOME}
-      />,
-    );
-
-    const sut = new ReactNavigationAdapter(navigation);
-
+  test('should call dispatch navigate action of React Navigation when call navigate', async () => {
+    const { sut, navigateSpy } = makeSut();
     sut.navigate(Routes.HOME, { any: 'any_params' });
 
-    expect(navigateSpy).toHaveBeenCalledTimes(1);
-    expect(navigateSpy).toHaveBeenCalledWith(Routes.HOME, {
-      any: 'any_params',
+    await waitFor(() => {
+      expect(navigateSpy).toHaveBeenCalledTimes(1);
+      expect(navigateSpy).toHaveBeenCalledWith(Routes.HOME, {
+        any: 'any_params',
+      });
     });
   });
 });
 
-class ReactNavigationAdapter implements NavigateScreen {
-  constructor(readonly navigation: NavigationContainerRef<any>) {}
-  navigate(routeName: string, params?: GenericObject | undefined): void {
-    this.navigation.dispatch(CommonActions.navigate(routeName, params));
-  }
-}
+const makeSut = () => {
+  let navigation = {} as NavigationContainerRef<any>;
+  const navigateSpy = jest.spyOn(CommonActions, 'navigate');
+
+  render(
+    <Navigation
+      setNavigationTop={(navigationRef) => (navigation = navigationRef)}
+      initialRouteName={Routes.WELCOME}
+    />,
+  );
+
+  const sut = new ReactNavigationAdapter(navigation);
+
+  return { sut, navigateSpy };
+};
