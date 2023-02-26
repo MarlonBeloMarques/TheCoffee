abbf194199df707d813f62439fff505df64ca319
Marlon Marques <marlon.belohd@gmail.com>
2023-02-26 18:17:38 -0300

    test(ui/purchase): refactor tests with makeSut

--- tests/ui/screens/purchase/purchase.spec.tsx
+++ tests/ui/screens/purchase/purchase.spec.tsx
@@ -5,101 +5,73 @@ import Purchase from '../../../../src/presentation/screens/purchase';
 
 describe('UI: Purchase', () => {
   test('should show coffee name with success', () => {
-    const coffee = getSelectedOptionItemStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={getPaymentDetailStub()}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      coffee,
+      sut: { getByTestId },
+    } = makeSut();
     const coffeeName = getByTestId('coffee_name_id');
+
     expect(coffeeName.props.children).toEqual(coffee.coffeeName);
   });
 
   test('should show price of coffee in correct pattern', () => {
-    const coffee = getSelectedOptionItemStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={getPaymentDetailStub()}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      coffee,
+      sut: { getByTestId },
+    } = makeSut();
     const coffeePrice = getByTestId('coffee_price_id');
+
     expect(coffeePrice.props.children).toEqual(
       `R$ ${coffee.coffeePrice.toFixed(2)}`,
     );
   });
 
   test('should show image of coffee with success', () => {
-    const coffee = getSelectedOptionItemStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={getPaymentDetailStub()}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      coffee,
+      sut: { getByTestId },
+    } = makeSut();
     const coffeeImage = getByTestId('coffee_image_id');
     expect(coffeeImage.type).toEqual('Image');
     expect(coffeeImage.props.source).toEqual(coffee.coffeeImage);
   });
 
   test('should show payment description with success', () => {
-    const coffee = getSelectedOptionItemStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={getPaymentDetailStub()}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      sut: { getByTestId },
+    } = makeSut();
     const paymentDescription = getByTestId('payment_description_id');
+
     expect(paymentDescription.props.children).toEqual('Pay with credit card:');
   });
 
   test('should show payment number credit card with success', () => {
-    const coffee = getSelectedOptionItemStub();
-    const paymentDetail = getPaymentDetailStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={paymentDetail}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      paymentDetail,
+      sut: { getByTestId },
+    } = makeSut();
     const numberCreditCard = getByTestId('payment_number_credit_card_id');
+
     expect(numberCreditCard.props.children).toEqual(
       paymentDetail.creditCard.number,
     );
   });
 
   test('should show payment icon credit card with success', () => {
-    const coffee = getSelectedOptionItemStub();
-    const paymentDetail = getPaymentDetailStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={paymentDetail}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      sut: { getByTestId },
+    } = makeSut();
     const iconCreditCard = getByTestId('payment_icon_credit_card_id');
+
     expect(iconCreditCard).toBeTruthy();
   });
 
   test('should show confirm purchase button with correct description', () => {
-    const coffee = getSelectedOptionItemStub();
-    const paymentDetail = getPaymentDetailStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={paymentDetail}
-        confirmPurchase={() => {}}
-      />,
-    );
+    const {
+      sut: { getByTestId },
+    } = makeSut();
     const confirmPurchaseButton = getByTestId('confirm_purchase_button_id');
+
     expect(confirmPurchaseButton).toBeTruthy();
     const descriptionButton = confirmPurchaseButton.props.children[0];
     expect(descriptionButton.props.children).toEqual('Confirm purchase');
@@ -107,15 +79,9 @@ describe('UI: Purchase', () => {
 
   test('should call confirmPurchase when press the button', () => {
     const confirmPurchase = jest.fn();
-    const coffee = getSelectedOptionItemStub();
-    const paymentDetail = getPaymentDetailStub();
-    const { getByTestId } = render(
-      <Purchase
-        coffeeSelected={coffee}
-        paymentDetail={paymentDetail}
-        confirmPurchase={confirmPurchase}
-      />,
-    );
+    const {
+      sut: { getByTestId },
+    } = makeSut(confirmPurchase);
     const confirmPurchaseButton = getByTestId('confirm_purchase_button_id');
 
     fireEvent.press(confirmPurchaseButton);
@@ -123,6 +89,21 @@ describe('UI: Purchase', () => {
     expect(confirmPurchase).toHaveBeenCalledTimes(1);
   });
 });
+
+const makeSut = (confirmPurchase = () => {}) => {
+  const coffee = getSelectedOptionItemStub();
+  const paymentDetail = getPaymentDetailStub();
+
+  const sut = render(
+    <Purchase
+      coffeeSelected={coffee}
+      paymentDetail={paymentDetail}
+      confirmPurchase={confirmPurchase}
+    />,
+  );
+
+  return { sut, paymentDetail, coffee };
+};
 
 const getPaymentDetailStub = () => {
   return {
